9ae8484fc25bcfe7f9b9d5937c0d8e334d25782e
kohei-kohei <sky6sk212@gmail.com>
2021-02-18 11:30:58 +0900

    feat: footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,20 @@
 <template>
   <v-footer dark padless>
-    <v-card
-      flat
-      tile
-      width="100%"
-      class="indigo lighten-1 white--text text-center"
-    >
-      <v-card-text class="white--text">
+    <v-card flat tile width="100%" class="purple lighten-4 text-center">
+      <v-card-text class="subtitle-1 black--text pb-2">
+        This web page is created by
+        <v-icon color="green darken-2">
+          mdi-vuejs
+        </v-icon>
+        <v-icon color="light-blue lighten-2">
+          mdi-vuetify
+        </v-icon>
+        <v-icon color="blue">
+          mdi-language-typescript
+        </v-icon>
+      </v-card-text>
+
+      <v-card-text class="black--text pt-2">
         &copy; 2021 — <strong>Kohei</strong>
       </v-card-text>
     </v-card>
